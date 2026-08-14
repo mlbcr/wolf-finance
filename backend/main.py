7485dbccd9f89c6a401c3c3e5558ba03453f8b98
mlbcr <malubacari@gmail.com>
2026-08-14 20:51:35 -0300

    Added new allowed origin

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "gowolffinance.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
