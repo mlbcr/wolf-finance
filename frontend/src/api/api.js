01a8641a507af07378497e92971b697027de1460
mlbcr <malubacari@gmail.com>
2026-08-15 22:54:52 -0300

    Updated api_url to render

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,5 @@
-export const API_URL = 'http://127.0.0.1:8000'
+// export const API_URL = 'http://127.0.0.1:8000'
+export const API_URL = 'https://wolf-finance.onrender.com'
 export * from './apiAuth'
 export * from './apiAlunos'
 export * from './apiEquipes'
